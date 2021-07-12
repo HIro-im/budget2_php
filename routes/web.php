9fe2d2920d37b4a41e94c96eb6476ef8a5eff060
Hiro-im <doraemon.for.private@gmail.com>
2021-07-12 18:30:38 +0900

    タイトル文字のリンクをホームに遷移するよう変更(認証してないとログイン画面へ)

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,4 @@ Route::group(['prefix' => 'calc', 'middleware' => 'auth'], function(){
 //Laravelで作成した認証のルート
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
